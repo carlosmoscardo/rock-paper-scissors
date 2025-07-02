cd7632df442dcb96b33394c2940ed99251c90a00
carlosmoscardo <carlosmoscardo02@gmail.com>
2025-07-02 09:30:16 +0200

    Normalize computer choice value to lowercase

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice === 0) {
-        return "Rock";
+        return "Rock".toLowerCase();
     } else if (computerChoice === 1) {
-        return "Paper";
-    } return "Scissors";
+        return "Paper".toLowerCase();
+    } return "Scissors".toLowerCase();
 }
 
 function getHumanChoice() {
