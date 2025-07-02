1f471529ceac0e772d420b5f3f1a50267b405557
carlosmoscardo <carlosmoscardo02@gmail.com>
2025-07-02 11:33:16 +0200

    Add a function to play one full round

--- script.js
+++ script.js
@@ -24,3 +24,26 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoiceRound, computerChoiceRound) {
+    if (humanChoiceRound === computerChoiceRound) {
+        humanScore += 0;
+        computerScore += 0;
+        console.log("Tie!");
+    } else if (humanChoiceRound === "rock" && computerChoiceRound === "paper"
+        || humanChoiceRound === "paper" && computerChoiceRound === "scissors"
+        || humanChoiceRound === "scissors" && computerChoiceRound === "rock"
+    ) {
+        humanScore += 0;
+        computerScore += 1;
+        console.log("You lose!");
+    } else {
+        humanScore += 1;
+        computerScore += 0;
+        console.log("You win!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
